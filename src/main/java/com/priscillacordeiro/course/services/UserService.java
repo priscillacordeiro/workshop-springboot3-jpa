14df97a13eeb427eb16c76eff4d49a6e9fb3b641
Priscilla Cordeiro <priscillacordeiros@gmail.com>
2023-05-12 16:34:43 +0100

    Service layer, component registration

---
+++ src/main/java/com/priscillacordeiro/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.priscillacordeiro.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.priscillacordeiro.course.entities.User;
+import com.priscillacordeiro.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> object = userRepository.findById(id);
+		return object.get();
+	}
+
+}
