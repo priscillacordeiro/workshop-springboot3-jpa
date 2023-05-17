5d46c625c1d1cdc69ae5298871547c5d429e3306
Priscilla Cordeiro <priscillacordeiros@gmail.com>
2023-05-17 15:03:50 +0100

    Exception handling - findById

--- src/main/java/com/priscillacordeiro/course/services/UserService.java
+++ src/main/java/com/priscillacordeiro/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.priscillacordeiro.course.entities.User;
 import com.priscillacordeiro.course.repositories.UserRepository;
+import com.priscillacordeiro.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> object = userRepository.findById(id);
-		return object.get();
+		return object.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User object)	{
